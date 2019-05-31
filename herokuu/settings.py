46d1ab48bed0ffe7b5c344a829f40863e48432a5
Lee <ltr0121@naver.com>
2019-06-01 00:32:07 +0900

    settings.py 수정

--- herokuu/settings.py
+++ herokuu/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fcz+ya)7f6jpr2qs5&(%@(ghqhh(ay
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
