bea0e12af0945917c744ef7dc9837a18c8ba1809
Lee <ltr0121@naver.com>
2019-06-01 00:11:01 +0900

    gitignore 수정

--- herokuu/settings.py
+++ herokuu/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fcz+ya)7f6jpr2qs5&(%@(ghqhh(ayz0=-qxdam=r$@4=@o881'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECRET_KEY = 'fcz+ya)7f6jpr2qs5&(%@(ghqhh(ayz0=-qxdam=r$@4=@o881'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fcz+ya)7f6jpr2qs5&(%@(ghqhh(ayz0=-qxdam=r$@4=@o881')
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
